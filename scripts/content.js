eb30d77520336c7eb7074adf102dbc8f2aa06574
Alkaison <505ganeshmourya@gmail.com>
2023-06-25 14:15:07 +0530

    Feat: content script and storage permission

---
+++ scripts/content.js
@@ -0,0 +1,16 @@
+// content.js
+
+// Capture keyboard events
+document.addEventListener("keydown", function (event) {
+    console.log("Keydown event:", event);
+});
+
+// Capture mouse click events
+document.addEventListener("click", function (event) {
+    console.log("Click event:", event);
+});
+
+// Capture form input events
+document.addEventListener("input", function (event) {
+    console.log("Input event:", event);
+});

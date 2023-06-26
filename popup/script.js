630f2e4141901b565921716644ff01df7c368896
Alkaison <505ganeshmourya@gmail.com>
2023-06-26 22:59:13 +0530

    Feat: task addition buttons

--- popup/script.js
+++ popup/script.js
@@ -1,1 +1,5 @@
-// Link Saver - soon...
+const githubBtn = document.querySelector("#githubBtn");
+
+githubBtn.addEventListener("click", () => {
+    window.open("https://github.com/Alkaison/Link-Saver-Extension/");
+});

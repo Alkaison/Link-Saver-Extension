3fe124239f7b6519fce6adb6b823c27565ba39b2
Alkaison <505ganeshmourya@gmail.com>
2023-07-04 21:35:13 +0530

    Feat: content script code for input detection

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,40 @@
-// content.js
+let intervalTimer;
 
-// Capture keyboard events
-document.addEventListener("keydown", function (event) {
-    console.log("Keydown event:", event);
-});
+// Load key-value pairs data from Chrome local storage 
+const loadData = () => {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(null, (result) => {
+            if (result === null || result === undefined)
+                resolve([]);
+            else
+                resolve(result);
+        });
+    });
+};
 
-// Capture mouse click events
-document.addEventListener("click", function (event) {
-    console.log("Click event:", event);
-});
+// Check if the input element is a text box 
+const checkInputElement = async (e) => {
+    
+    const isInputBox = e.target.tagName === "INPUT";
+    const isTextBox = e.target.tagName === "TEXTAREA";
 
-// Capture form input events
-document.addEventListener("input", function (event) {
-    console.log("Input event:", event);
+    if (isInputBox || isTextBox) {
+        const inputElement = e.target;
+        const value = inputElement.value;
+
+        const getIndexOfSign = value.lastIndexOf("$") + 1;
+
+        if (getIndexOfSign !== -1) {
+            const jsonData = await loadData();
+            const parsedData = JSON.parse(JSON.stringify(jsonData));
+            console.log(parsedData);
+        }
+        console.log("Done 100!");
+    }
+};
+
+// Capture form text input events
+document.addEventListener("input", (e) => {
+    clearInterval(intervalTimer);
+    intervalTimer = setTimeout(() => checkInputElement(e), 100);
 });
